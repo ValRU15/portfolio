5eb695fc8e82249a977b74616890b01595764e6e
Valentina Ramirez <valentinar@makata.tv>
2023-11-16 21:37:53 -0500

    cursor

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,32 @@ import './App.css';
 import { NavBar } from './components/NavBar/NavBar'; 
 import { Bio } from './components/Bio/Bio'; 
 import { Education } from './components/Education/Education';
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <>
+    <AnimatedCursor
+      innerSize={15}
+      outerSize={25}
+      color='245, 95, 115'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+    />
     <NavBar />
     <Bio />
     <Education />
